a2e72d101b11929dfe503c6819c6a11cb4f7b3bd
Eunjae TonyLee <lpl2001@naver.com>
2023-02-28 22:47:09 +0900

    Main 페이지를 추가했어

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserMenu from "./components/UserMenu";
 import Button from "./components/Button";
 import { useState } from 'react';
 import './App.css';
 import Modal from './components/Modal';
+import Main from "./pages/Main";
 
 
 function App() {
@@ -11,19 +13,24 @@ function App() {
   let [modal, setModal] = useState(false);
   return (
     <div className="App">
+      <BrowserRouter>
       <div className="Home-nav">
         <div className="Logo">Art-ZIP</div>
         <div className="Menu-wrapper">
           <div className="Menu">공연정보</div>
           <div className="Menu">공연시설정보</div>
           <UserMenu/>
-          <Button></Button>
         </div>
       </div>
       <h4><span onClick={() => { setLikes(likes + 1); modal === true ? setModal(false) : setModal(true); }}>👍🏻</span>{likes}</h4>
       {
         modal === true ? <Modal /> : null
       }
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </div >
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+    return(
+        <div className="Main-wrapper">
+            메인페이지 입니다.
+        </div>
+    )
+}
+
+export default Main;
