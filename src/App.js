4980d191ca528bf275d30f46957b65041557cf56
ej-rarus <lpl2001@naver.com>
2023-02-28 16:53:13 +0900

    modal창 만들기 연습

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Modal from './components/Modal';
 
 function App() {
   let [likes, setLikes] = useState(0);
-
+  let [modal, setModal] = useState(false);
   return (
     <div className="App">
       <div className="Home-nav">
         안녕
       </div>
-      <h4><span onClick={() => { setLikes(likes + 1) }}>👍🏻</span>{likes}</h4>
-      <Modal></Modal>
-
+      <h4><span onClick={() => { setLikes(likes + 1); modal === true ? setModal(false) : setModal(true); }}>👍🏻</span>{likes}</h4>
+      {
+        modal === true ? <Modal /> : null
+      }
     </div >
   );
 }
